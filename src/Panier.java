fbeb1eb580415c484c10630eae59a43b7804d0ec
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-04-09 11:02:30 +0200

    Ventes

--- src/Panier.java
+++ src/Panier.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 public class Panier {
     public Date paiement;
     public Double valeur;
+    public Client acheteur;
 
     public Map<Gamme, Integer> achats;
+    public Livraison livraison;
 
     public Panier() {
         this.paiement = new Date();
@@ -16,5 +18,20 @@ public class Panier {
     public void achete(Gamme produits, Integer quantite) {
         achats.put(produits, quantite);
         valeur += produits.prix * quantite;
+    }
+
+    public void declencheLivraison() {
+        this.livraison = new Livraison();
+
+        // Itere achats
+        for (Gamme g : achats.keySet()) {
+            Integer quantite = achats.get(g);
+
+            for (int i = 0; i < quantite; i++) {
+                this.livraison.ajoute(g.vendProduit());
+            }
+        }
+
+        this.livraison.demarre();
     }
 }
