2c4f622f1658174a19aa0adfb584db0bae922a2c
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-04-09 09:41:37 +0200

    Recherche modèle UML

---
+++ src/Personne.java
@@ -0,0 +1,9 @@
+public class Personne {
+    public String nom;
+    public String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
