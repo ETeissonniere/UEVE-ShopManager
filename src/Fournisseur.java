a96559fc7c28ddeb5b928fce6a148c3807848eef
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-04-09 10:05:39 +0200

    Fournisseur

---
+++ src/Fournisseur.java
@@ -0,0 +1,11 @@
+public class Fournisseur {
+    public String siret;
+    public String nom;
+    public String adresse;
+
+    public Fournisseur(String siret, String nom, String adresse) {
+        this.siret = siret;
+        this.nom = nom;
+        this.adresse = adresse;
+    }
+}
