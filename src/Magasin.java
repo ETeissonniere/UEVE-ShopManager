076ad03bcfd5a811a6e11455b959adb2e78f8a84
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-04-23 10:45:06 +0200

    configure gradle

--- src/Magasin.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-import java.util.ArrayList;
-
-public class Magasin {
-    public List<Employe> employes;
-    public List<Panier> paniers;
-    public List<Fournisseur> fournisseurs;
-}
---
+++ src/main/java/UEVEZon/models/Magasin.java
@@ -0,0 +1,43 @@
+import java.sql.*;
+
+public class Magasin {
+    private Connection conn;
+
+    public Magasin(Connection conn) {
+        this.conn = conn;
+        createTablesIfNeeded();
+    }
+
+    public static Magasin create(String path) {
+        String url = "jdbc:sqlite:C:/sqlite/db/" + path;
+
+        conn = DriverManager.getConnection(url);
+        return new Magasin(conn);
+    }
+
+    public static Magasin connect(String path) {
+        String url = "jdbc:sqlite:C:/sqlite/db/" + path;
+
+        Connection conn = DriverManager.getConnection(url);
+        return new Magasin(conn);
+    }
+
+    public void close() {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    private void createTablesIfNeeded() {
+        String sql = ""
+            + "CREATE TABLE IF NOT EXISTS employes (id INTEGER PRIMARY KEY AUTO_INCREMENT, nom TEXT NOT NULL, prenom TEXT NOT NULL, role TEXT NOT NULL, salaire DECIMAL(10, 10) NOT NULL);\n"
+            + "CREATE TABLE IF NOT EXISTS clients (id INTEGER PRIMARY KEY AUTO_INCREMENT, nom TEXT NOT NULL, prenom TEXT NOT NULL, email TEXT NOT NULL);\n"
+            + "CREATE TABLE IF NOT EXISTS gammes (id INTEGER PRIMARY KEY AUTO_INCREMENT, codeBarre TEXT NOT NULL, prix DECIMAL(10, 5) NOT NULL);\n"
+            + "CREATE TABLE IF NOT EXISTS produits (id INTEGER PRIMARY KEY AUTO_INCREMENT, dateEntree TEXT NOT NULL, numSerie TEXT NOT NULL);\n"
+            + "CREATE TABLE IF NOT EXISTS fournisseurs (id INTEGER PRIMARY KEY AUTO_INCREMENT, siret TEXT NOT NULL, nom TEXT NOT NULL, addr TEXT NOT NULL);\n"
+            + "CREATE TABLE IF NOT EXISTS paniers (id INTEGER PRIMARY KEY AUTO_INCREMENT, datePaiement TEXT, dateReception TEXT, etat TEXT NOT NULL, valeur DECIMAL(10, 10) NOT NULL);";
+
+        Statement stmt = conn.createStatement();
+        stmt.execute(sql);
+    }
+}
