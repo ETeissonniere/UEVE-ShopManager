1dd66c1ce3b68f400d613e158d7314a19b260077
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-05-08 08:38:02 +0200

    tabs

---
+++ src/main/java/UEVEZon/views/EmployeView.java
@@ -0,0 +1,9 @@
+package UEVEZon.views;
+
+import javax.swing.*;
+import UEVEZon.models.*;
+
+public class EmployeView extends JPanel {
+	public EmployeView(Magasin mag) {
+	}
+}
--- src/main/java/UEVEZon/views/MainView.java
+++ src/main/java/UEVEZon/views/MainView.java
@@ -1,19 +1,22 @@
 package UEVEZon.views;
 
 import javax.swing.*;
+import java.awt.GridLayout;
 import UEVEZon.models.*;
 
 public class MainView {
 	JFrame window;
 
 	public MainView(Magasin mag) {
-		window = new JFrame();
 		JTabbedPane tabs = new JTabbedPane();
+		tabs.add("Employes", new EmployeView(mag));
+		tabs.add("mock", new EmployeView(mag));
 
-		window.add(tabs);
-		window.setSize(1200,800);
-		window.setLayout(null);
+		window = new JFrame();
+		window.setSize(1200, 800);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setLayout(new GridLayout(1, 1));
+		window.getContentPane().add(tabs);
 	}
 
 	public void show() {
